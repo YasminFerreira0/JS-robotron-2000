c92e68d9716949a3ddc1b008c60e3929d98c1964
Yasmin Ferreira <yasmincferreira8@gmail.com>
2022-12-07 14:23:31 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector('#robotron');
+
+robotron.addEventListener("click", function() {
+    console.log("Cliquei no robo!!")
+});
+
+function dizOi () {
+    console.log()
+    console.log()
+}
+
+dizOi();
