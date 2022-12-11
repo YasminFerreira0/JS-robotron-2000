6f5b87b36875825affde4212100628302ba8014d
Yasmin Ferreira <yasmincferreira8@gmail.com>
2022-12-10 22:47:50 -0400

    up

--- main.js
+++ main.js
@@ -2,15 +2,27 @@
 robotron.addEventListener("click", function() {
     console.log("Cliquei no robo!!")
 });*/
+//const subitrair = document.querySelector('#subtrair');
+//const somar = document.querySelector('#somar');
+//const braco = document.querySelector('#braco');
 
-const subitrair = document.querySelector('#subtrair');
-const braco = document.querySelector('#braco');
-const somar = document.querySelector('#somar');
 
-subitrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1;
+const controle = document.querySelectorAll("[data-controle]");
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+    });
+    
 });
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1;
-});
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    }else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
+
